14fa20fbd41670272a26cf33bd720d4942e01085
Paulo Victor Silva <paulpbi16@gmail.com>
2024-11-27 09:33:32 -0300

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>Code by for Paulo Victor - 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
