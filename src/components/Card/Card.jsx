920efd97a61f1e5e73f5c709ba6d35e4985b8a19
Paulo Victor Silva <paulpbi16@gmail.com>
2024-11-27 17:17:26 -0300

    chore: add card proptypes, styles and navegation to other pages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,29 +1,35 @@
+import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
+import Proptypes from "prop-types";
 
-const Card = ({ character }) => {
-  console.log(character);
-
+const Card = ({ data }) => {
   return (
     <section className={styles.card}>
-      {character &&
-        character.results.map(({ id, name, status, image }) => (
+      {data &&
+        data.map(({ id, name, status, image }) => (
           <div key={id} className={styles.character}>
-            <img
-              src={image}
-              alt={name}
-              height={168}
-              width={240}
-              className={styles.image}
-            />
+            <Link to={`character/${id}`} className={styles.links}>
+              <img
+                src={image}
+                alt={name}
+                height={170}
+                width={240}
+                className={styles.image}
+              />
 
-            <div className={styles.texts}>
-              <h1 className={styles.title}>{name}</h1>
-              <p className={styles.status}>{status}</p>
-            </div>
+              <div className={styles.texts}>
+                <h1 className={styles.title}>{name}</h1>
+                <p className={styles.status}>{status}</p>
+              </div>
+            </Link>
           </div>
         ))}
     </section>
   );
+};
+
+Card.propTypes = {
+  data: Proptypes.array,
 };
 
 export default Card;
