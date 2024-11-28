95aead1686e5fea3078ffdd3510088757d7ff739
Paulo Victor Silva <paulpbi16@gmail.com>
2024-11-28 14:59:27 -0300

    feat: add error handler message

--- src/helpers/Error/Error.jsx
+++ src/helpers/Error/Error.jsx
@@ -1,13 +1,23 @@
+import Proptypes from "prop-types";
 import styles from "./Error.module.css";
+import { motion } from "motion/react";
 
 const Error = ({ error }) => {
   return (
-    <div className={styles.container}>
+    <motion.div
+      initial={{ translateY: "-50px", scale: 0.5, opacity: 0 }}
+      animate={{ translateY: "0px", scale: 1, opacity: 1 }}
+      className={styles.container}
+    >
       <div className={styles.content}>
         <strong className={styles.error}>{error}</strong>
       </div>
-    </div>
+    </motion.div>
   );
+};
+
+Error.propTypes = {
+  error: Proptypes.string,
 };
 
 export default Error;
